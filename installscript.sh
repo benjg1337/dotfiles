f718bb9351af47a875d04f9efc4c005d08ee7cbb
benji <129755752+benjg1337@users.noreply.github.com>
2024-06-24 21:13:51 +0100

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-LIST_OF_APPS="htop, lsd, bat, ncdu, fzf, cmus, tmux, neovim, neofetch, curl, feh, python3-pip, john, hashcat, tor, tree-sitter, tree, nmap, wget, wireshark, git"
+LIST_OF_APPS="htop, lsd, bat, ncdu, fzf, cmus, tmux, neovim, neofetch, curl, feh, python3-pip, john, hashcat, tor, tree-sitter, tree, nmap, wget, wireshark, git, timeshift"
 
 brew update -y && brew upgrade -y
 brew install $LIST_OF_APPS
