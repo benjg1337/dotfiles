d20e2f58a95c9bc9a2dd2fa914b6d6794c7288bb
benji <129755752+benjg1337@users.noreply.github.com>
2025-02-06 14:04:36 +0000

    Update macinstall.sh

--- macinstall.sh
+++ macinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-LIST_OF_APPS="htop, lsd, bat, ncdu, fzf, cmus, tmux, neovim, neofetch, curl, feh, python3-pip, john, hashcat, tor, tree-sitter, tree, nmap, wget, wireshark, git, timeshift"
+LIST_OF_APPS="htop, lsd, bat, ncdu, fzf, cmus, tmux, neovim, neofetch, curl, feh, python3-pip, john, hashcat, tor, tree-sitter, tree, nmap, wget, wireshark, git, timeshiftl, lnav"
 
 brew update -y && brew upgrade -y
 brew install $LIST_OF_APPS
